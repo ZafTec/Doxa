31dc54cbc478945970fa1d1486e8314e24d2cf5e
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-03-23 14:20:13 +0300

    Defined the interfaces, and implemented the methods for the category repository

---
+++ backend/repository/category-repository.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"doxa_backend/models"
+	"gorm.io/gorm"
+)
+
+type ICategoryRepository interface {
+	Create(category models.Category) (models.Category, error)
+	Update(category models.Category) (models.Category, error)
+	GetById(categoryId uint64) (models.Category, error)
+	GetAll() ([]models.Category, error)
+}
+
+type CategoryRepository struct {
+	connection *gorm.DB
+}
+
+func (c *CategoryRepository) Create(category models.Category) (models.Category, error) {
+
+	result := c.connection.Save(&category)
+	if result.Error != nil {
+		return models.Category{}, result.Error
+	}
+	return category, nil
+}
+
+func (c *CategoryRepository) Update(category models.Category) (models.Category, error) {
+
+	result := c.connection.Save(&category)
+	if result.Error != nil {
+		return models.Category{}, result.Error
+	}
+	return category, nil
+}
+
+func (c *CategoryRepository) GetById(categoryId uint64) (models.Category, error) {
+	var category models.Category
+	result := c.connection.First(&category, categoryId)
+	if result.Error != nil {
+		return models.Category{}, result.Error
+	}
+	return category, nil
+}
+
+func (c *CategoryRepository) GetAll(categoryId uint64) ([]models.Category, error) {
+	var categories []models.Category
+	result := c.connection.First(&categories, categoryId)
+	if result.Error != nil {
+		return []models.Category{}, result.Error
+	}
+	return categories, nil
+}
