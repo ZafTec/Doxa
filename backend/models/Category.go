03eae49320ed7dd9d3a6f04baea5d8ca560e4f2d
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-03-23 13:30:46 +0300

    Added the ItemVariant model

--- backend/models/Category.go
+++ backend/models/Category.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type Category struct {
-	CategoryID uint64    `gorm:"primary_key" json:"category_id"`
+	CategoryId uint64    `gorm:"primary_key" json:"category_id"`
 	Name       string    `gorm:"type:varchar(255)" json:"name"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
+	Items      []Item    `gorm:"foreignKey:CategoryId"`
 }
