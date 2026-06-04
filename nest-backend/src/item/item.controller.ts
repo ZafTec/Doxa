775978ce081329f71d6a245ab78d00d0dec9542a
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-06-04 17:54:30 +0300

    Added filtering the items by brand and category

--- nest-backend/src/item/item.controller.ts
+++ nest-backend/src/item/item.controller.ts
@@ -1,5 +1,7 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post, UsePipes, Query} from '@nestjs/common';
 import {ItemService} from "./item.service";
+import {ZodValidationPipe} from "nestjs-zod";
+import {type CreateItemDto, createItemSchema, getItemQuerySchema, type ItemQueryDto} from "./item.schema";
 
 @Controller('item')
 export class ItemController {
@@ -7,7 +9,17 @@ export class ItemController {
     }
 
     @Get()
-    async getAll() {
-        return this.itemService.getAll()
+    async getAll(
+        @Query(new ZodValidationPipe(getItemQuerySchema)) queryParams: ItemQueryDto
+    ) {
+        console.log("The query parameters are ",queryParams)
+        return this.itemService.getAll(queryParams)
+    }
+
+    @Post('/create')
+    async create(
+        @Body(new ZodValidationPipe(createItemSchema)) item: CreateItemDto,
+    ) {
+        return this.itemService.createItem(item);
     }
 }
--- nest-backend/src/item/item.service.ts
+++ nest-backend/src/item/item.service.ts
@@ -1,11 +1,47 @@
-import {Get, Injectable} from '@nestjs/common';
+import {BadRequestException, Get, Injectable} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
+import {type CreateItemDto, type ItemQueryDto} from "./item.schema";
+import type {Category, Item} from "../../prisma/generated/client";
+import type {Prisma} from "../../prisma/generated/client";
 
 @Injectable()
 export class ItemService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async getAll(){
-        return this.prisma.item.findMany();
+    private buildItemFilters(filters: ItemQueryDto): Prisma.ItemWhereInput {
+        const where: Prisma.ItemWhereInput = {}
+        if (filters.brand){
+            where.brand = {
+                in: filters.brand
+            }
+        }
+        if (filters.category){
+            where.category = {
+                name: {
+                    in: filters.category
+                }
+            }
+        }
+        return where;
+    }
+
+    async getAll(filters: ItemQueryDto): Promise<Item[]>{
+        return this.prisma.item.findMany({
+            where: this.buildItemFilters(filters)
+        });
+    }
+
+    async createItem(item: CreateItemDto) {
+        const category: Category | null = await this.prisma.category.findUnique({
+            where: {
+                id: item.categoryId
+            }
+        })
+        if (!category) {
+            throw new BadRequestException("Category not found");
+        }
+
+        const createdItem: Item = await this.prisma.item.create({data: item})
+        return createdItem;
     }
 }
